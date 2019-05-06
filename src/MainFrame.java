ab96f63f2acd55665d2bf2dafd4f90cf0f4b3e4c
Andre <drdesignerpro@gmail.com>
2019-05-06 18:37:11 -0300

    Commit inicial

---
+++ src/MainFrame.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    private JTextArea textArea;
+    private JButton button;
+
+
+    public MainFrame() {
+        super("Hello World");
+
+        setLayout(new BorderLayout());
+
+        textArea = new JTextArea();
+        button = new JButton("Click here");
+
+        add(textArea, BorderLayout.CENTER);
+        add(button, BorderLayout.SOUTH);
+
+        setSize(600, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
